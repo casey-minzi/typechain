5b92fb291ec28d7ae083a08cc0b50022da81580e
casey-minzi <cluvhope@gmail.com>
2020-05-28 15:35:45 +0100

    #4 Types in Typescript (install tsc-watch)

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const name = "Casey", age = "25", gender = "female";
-const sayHi = (name, age, gender) => {
-    console.log(`Hello, ${name}, you are ${age} years old ${gender}.`);
-};
-sayHi(name, age, gender);
-//# sourceMappingURL=index.js.map
