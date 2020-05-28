60b5aaefdf2d8386f2a85c8629bd12d75aea7d0b
casey-minzi <cluvhope@gmail.com>
2020-05-28 16:00:02 +0100

    #6 Classes on Typescript pt1

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
-interface Human {
-    name: string;
-    age: number;
-    gender: string;
-};
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
 
-const person = {
-    name: 'Casey',
-    age: 25,
-    gender: 'female'
-};
+const caden = new Human("Caden", 24, "female");
 
 const sayHi = (person: Human): string => {
     let {name, age, gender} = person;
     return `Hello, ${name}, you are ${age} years old ${gender}.`;
 }
 
-console.log(sayHi(person));
+console.log(sayHi(caden));
 
 export {};
