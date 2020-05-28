0da899785803f8fed0d09427ccaf73f984c14360
casey-minzi <cluvhope@gmail.com>
2020-05-28 15:47:40 +0100

    #5 Interfaces on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
-const sayHi = (name: string, age: number, gender: string): string => {
+interface Human {
+    name: string;
+    age: number;
+    gender: string;
+};
+
+const person = {
+    name: 'Casey',
+    age: 25,
+    gender: 'female'
+};
+
+const sayHi = (person: Human): string => {
+    let {name, age, gender} = person;
     return `Hello, ${name}, you are ${age} years old ${gender}.`;
 }
 
-console.log(sayHi('Casey', 25, 'female'));
+console.log(sayHi(person));
 
 export {};
